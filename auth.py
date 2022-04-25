e0397c4e1475b3c74060ef4fb75f260e89f838c7
Lucy <Luciekinyanjui>
2022-04-26 01:38:40 +0300

    auth function

--- auth.py
+++ auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -7,10 +9,28 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('Login.html')
 
-@auth.route('/signup')
-def signup():
-    return render_template('Signup.html')
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    # code to validate and add user to database goes here
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
 
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    if user: # if a user is found, we want to redirect back to signup page so user can try again
+        return redirect(url_for('auth.signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    
+  return redirect(url_for('auth.login'))
+  
 @auth.route('/logout')
 def logout():
     return 'Logout'
